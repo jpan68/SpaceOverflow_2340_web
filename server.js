ce5194f9848c8ae0dd6d54281709371a2c5d9bc3
Jessica <jpan68@gatech.edu>
2019-03-18 18:33:34 -0400

    Set EJS as primary template engine, add express-partials middleware

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
+const partials = require('express-partials');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+
+// load the express-partials middleware
+app.use(partials());
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// imports controllers/index.js
 app.use(require('./controllers'));
 
 app.listen(port, () => {
